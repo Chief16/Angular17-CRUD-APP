1259ea864262c78e64be5e99af1749fa2080bf94
Bittu <bittu1040@gmail.com>
2024-08-18 18:24:41 +0530

    Restructured components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateComponent } from './create/create.component';
-import { ReadComponent } from './read/read.component';
-import { UpdateComponent } from './update/update.component';
-import { DeleteComponent } from './delete/delete.component';
+
 
 const routes: Routes = [
-  { path: 'create', component: CreateComponent },
-  { path: 'read', component: ReadComponent },
-  { path: 'update', component: UpdateComponent },
-  { path: 'delete', component: DeleteComponent },
+
 ];
 
 @NgModule({
